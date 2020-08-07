712e842c6133b6db9c1aac7f2a65e175a33972f8
dg0397 <rgg0397@gmail.com>
2020-08-06 23:53:04 -0400

    Updates

---
+++ precache-manifest.e2a014df8a8bdf26f18eeb43eaaac588.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd2e83504d149eefd60ea4b008ba967d",
+    "url": "/React-JavaScript-Calculator/index.html"
+  },
+  {
+    "revision": "9ea6adec5d716735603a",
+    "url": "/React-JavaScript-Calculator/static/css/main.64c2005b.chunk.css"
+  },
+  {
+    "revision": "4d1712616edbad473f61",
+    "url": "/React-JavaScript-Calculator/static/js/2.ad1f9fa4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-JavaScript-Calculator/static/js/2.ad1f9fa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ea6adec5d716735603a",
+    "url": "/React-JavaScript-Calculator/static/js/main.fe567200.chunk.js"
+  },
+  {
+    "revision": "48272989ec3dfe910716",
+    "url": "/React-JavaScript-Calculator/static/js/runtime-main.8d2e336b.js"
+  }
+]);
